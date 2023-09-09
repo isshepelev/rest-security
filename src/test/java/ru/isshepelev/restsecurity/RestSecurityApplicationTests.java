f81d3984d738156d447c371ad31218b194b2115d
Ilya <isshepelev2004@gmail.com>
2023-09-10 01:14:41 +0300

    add SecurityConfiguration

---
+++ src/test/java/ru/isshepelev/restsecurity/RestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.restsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
